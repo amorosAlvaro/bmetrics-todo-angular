d49d4996a804709147f1198cbbb434863384dd4f
alvaro <amoros@bitsinmotion.de>
2022-05-17 11:46:39 +0200

    Add: Angular8 basic config

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>
+    Welcome to {{ title }}!
+  </h1>  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list-bmetric';
+  author = 'Álvaro Amorós';
+}
